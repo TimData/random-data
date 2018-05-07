89a33d811854cf9d118c235a8dccd2ca9e50f635
Tim <timspangler@outlook.com>
2018-05-07 11:14:08 -0400

    mentioned rev.

---
+++ R/rreverse.R
@@ -0,0 +1,16 @@
+#' reverse
+#'
+#' Reverse returns a copy of a vector whose elements are in the reverse order. The end.
+#'
+#'See also\code{\link{rev}}
+#'
+#' @param x
+#'
+#' @return a vector
+#' @export
+#'
+#' @examples
+#' reverse(1:10)#'
+reverse <- function(x) {
+  if (length(x) > 0)  x[length(x):1] else x
+}
